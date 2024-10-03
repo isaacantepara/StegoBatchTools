2d669851dea68581ddeee31335f53c27c480d83d
Isaac Antepara <69361351+isaacanteparac@users.noreply.github.com>
2024-10-03 02:32:31 -0500

    generacion docx /modificacion metadata /diagrama

---
+++ src/cambiarMetadata.py
@@ -0,0 +1,66 @@
+from docx import Document
+from datetime import datetime
+import os
+import time
+import random
+import subprocess
+
+def modificar_metadatos(archivo, autores):
+ 
+    """Modifica los metadatos de un archivo DOCX."""
+    doc = Document(archivo)
+    print(archivo)
+
+    # Elegir un autor aleatorio
+    autor_seleccionado = random.choice(autores)
+    line = autor_seleccionado.split(" ") 
+    nombre_completo = f"{line[0]}_{line[1]}"
+
+    # Convertir la cadena de texto a un objeto datetime
+    anio = f"{line[-1][0]}{line[-1][1]}{line[-1][2]}{line[-1][3]}"
+    target_datetime = datetime.strptime(f"{anio}:03:21 12:00:00", "%Y:%m:%d %H:%M:%S")
+
+
+    # Establecer el autor en el documento
+    doc.core_properties.author = nombre_completo
+
+     # Establecer metadatos con el objeto datetime
+    doc.core_properties.created = target_datetime
+    doc.core_properties.modified = target_datetime
+
+    doc.core_properties.description = "lil" 
+
+
+
+    # Guardar los cambios
+    doc.save(archivo)
+
+    # Actualizar las fechas del archivo con el timestamp
+    timestamp = int(time.mktime(target_datetime.timetuple()))
+    os.utime(archivo, (timestamp, timestamp))
+
+    # Modificar la fecha del nodo del archivo con exiftool
+    '''subprocess.run(["exiftool",
+                     "-FileInodeChangeDate=" + target_datetime.strftime("%Y:%m:%d %H:%M:%S"),
+                     archivo])
+
+    # Modificar la fecha de Zip Modify Date con exiftool
+    subprocess.run(["exiftool",
+                     "-ZipModifyDate=" + target_datetime.strftime("%Y:%m:%d %H:%M:%S"),
+                     archivo])'''
+
+
+
+# Leer la lista de autores desde el archivo txt
+with open("./registros_captura.txt", "r") as f:
+    autores = f.readlines()
+
+
+# Carpeta con los archivos DOCX
+carpeta_docx = "./informes_zoologico"  # Reemplaza con la ruta de tu carpeta
+
+# Iterar sobre los archivos DOCX en la carpeta
+for archivo in os.listdir(carpeta_docx):
+    if archivo.endswith(".docx"):
+        ruta_archivo = os.path.join(carpeta_docx, archivo)
+        modificar_metadatos(ruta_archivo, autores)
---
+++ src/generador_de_animales.py
@@ -0,0 +1,111 @@
+from docx import Document
+from faker import Faker
+import random
+import os
+from datetime import datetime
+import wikipedia
+
+# Inicializar Faker
+fake = Faker()
+
+# Lista de nombres de animales (ampliada) en inglés
+animales = [
+    'Lion', 'Asian Elephant', 'Giraffe', 'Tiger', 'Giant Panda', 
+    'Grevy\'s Zebra', 'Red Kangaroo', 'Hippopotamus', 'White Rhinoceros', 
+    'Bornean Orangutan', 'Nile Crocodile', 'American Flamingo', 'Emperor Penguin', 
+    'RedFox', 'Gray Wolf', 'Brown Bear', 'Burmese Python', 
+    'River Otter', 
+    'Puma', 'Eagle Owl', 'Kestrel', 'Western Gorilla', 'Common Chimpanzee', 
+    'Ring-tailed Lemur', 'Capuchin Monkey', 'Koala', 'Bottlenose Dolphin', 'Humpback Whale', 
+    'Great White Shark', 'Loggerhead Sea Turtle', 'Boa Constrictor', 'Goanna', 
+    'Green Iguana', 'Bullfrog', 'Common Toad', 'Monarch Butterfly', 'Honey Bee', 
+    'Silkworm', 'Dung Beetle', 'Woodpecker', 'Bald Eagle', 
+    'Peregrine Falcon', 'Herring Gull',
+    'Canada Goose', 'Mute Swan', 
+    'Common Quail', 'Rock Dove', 'Indian Peafowl', 'Common Pheasant', 
+    'Domestic Dog', 'Domestic Cat', 'European Rabbit', 'Horse', 
+    'Domestic Pig', 'Sheep', 'Goat', 'Donkey', 'Dromedary Camel', 
+    'Northern Elephant Seal', 'Harbor Seal', 'West Indian Manatee', 'Beaver', 
+    'Gray Squirrel', 'House Mouse', 'European Hedgehog', 'Bat', 
+    'Anna\'s Hummingbird', 'African Grey Parrot', 'Blue-and-yellow Macaw', 'Domestic Canary', 
+    'Crayfish', 'American Lobster', 'Common Octopus', 'Giant Squid', 
+    'Jellyfish', 'Starfish', 'Goldfish', 'Hammerhead Shark', 
+    'Blue Whale', 'Bottlenose Dolphin', 'Seahorse', 'Red Coral', 
+    'Green Algae', 'Plankton', 'Bacteria', 'Mushroom', 
+    'Houseplant', 'Oak Tree', 'Rose'
+]
+# Función para crear un informe para cada animal
+def generar_informe(animal):
+    # Crear un nuevo documento
+    doc = Document()
+    
+    # Obtener la fecha actual
+    fecha_actual = datetime.now()
+
+    # Título del informe
+    doc.add_heading(f'Informe de {animal}', 0)
+    
+    # Información general
+    doc.add_heading('Información General', level=1)
+    visitas = random.randint(5000, 20000)
+    doc.add_paragraph(f"El zoológico ha recibido un total de {visitas} visitantes en la fecha del informe.")
+    doc.add_paragraph(f"Este informe fue elaborado por: {fake.name()}") # Nombre del recolector
+    
+    # Información sobre el animal
+    doc.add_heading('Estado del Animal', level=1)
+    try:
+        # Busca información sobre el animal en Wikipedia (en inglés)
+        page = wikipedia.page(animal)
+        
+        # Extrae información relevante
+        nombre_cientifico = page.title
+        estado_conservacion = page.summary.split(" ")[0]  
+        descripcion = page.content  # Obtiene todo el contenido de la página
+        
+        # Asigna información a la sección del documento
+        doc.add_paragraph(f"Nombre científico: {nombre_cientifico}")
+        doc.add_paragraph(f"Estado de conservación: {estado_conservacion}")
+        doc.add_paragraph(descripcion)
+
+    except wikipedia.exceptions.PageError:
+        # Maneja el error si no se encuentra información
+        doc.add_paragraph(f"No se encontró información sobre {animal} en Wikipedia.")
+
+    # Información adicional (datos ficticios)
+    doc.add_heading('Información adicional', level=1)
+    doc.add_paragraph(f"Peso: {random.randint(10, 500)} kg")
+    doc.add_paragraph(f"Altura: {random.randint(1, 5)} m")
+    doc.add_paragraph(f"Longitud: {random.randint(1, 10)} m")
+    doc.add_paragraph(f"Velocidad máxima: {random.randint(10, 100)} km/h")
+    doc.add_paragraph(f"Esperanza de vida: {random.randint(5, 50)} años")
+    doc.add_paragraph(f"Número de crías por camada: {random.randint(1, 10)}")
+    doc.add_paragraph(f"Tiempo de gestación: {random.randint(1, 12)} meses")
+
+    # Agregar una sección con datos adicionales
+    doc.add_heading('Datos adicionales', level=1)
+    doc.add_paragraph(f"Distribución geográfica: {fake.sentence(nb_words=5)}")
+    doc.add_paragraph(f"Dieta: {fake.sentence(nb_words=3)}")
+    doc.add_paragraph(f"Amenazas: {fake.sentence(nb_words=5)}")
+    doc.add_paragraph(f"Medidas de conservación: {fake.sentence(nb_words=5)}")
+
+    # Agregar una sección con información sobre el cuidado en el zoológico
+    doc.add_heading('Cuidado en el zoológico', level=1)
+    doc.add_paragraph(f"Hábitat del zoológico: {fake.sentence(nb_words=5)}")
+    doc.add_paragraph(f"Dieta del zoológico: {fake.sentence(nb_words=5)}")
+    doc.add_paragraph(f"Programa de enriquecimiento ambiental: {fake.sentence(nb_words=5)}")
+    doc.add_paragraph(f"Programa de reproducción: {fake.sentence(nb_words=5)}")
+
+    # Guardar el documento
+    dia_random = random.randint(1, 31)  # Generar un día aleatorio
+    nombre_archivo = f"informes_zoologico/Informe_{animal.replace(' ', '_')}_{dia_random}_{fecha_actual.month}.docx"
+    doc.save(nombre_archivo)
+    print(f"Informe guardado como: {nombre_archivo}")
+
+# Directorio de salida
+output_dir = "informes_zoologico"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Generar informes para cada animal
+for animal in animales:
+    generar_informe(animal)
