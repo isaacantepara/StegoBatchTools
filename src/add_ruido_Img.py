708c78bcd48966882ec565feac1190ad27b8ddf1
Isaac Antepara <69361351+isaacanteparac@users.noreply.github.com>
2024-10-03 17:16:09 -0500

    ruido

---
+++ src/add_ruido_Img.py
@@ -0,0 +1,39 @@
+import base64
+import os
+import random
+from PIL import Image
+
+def imagen_a_base64(ruta_imagen):
+    """Convierte una imagen a base64."""
+    with open(ruta_imagen, "rb") as imagen_archivo:
+        imagen_binaria = imagen_archivo.read()
+        return imagen_binaria  # No decodificamos a utf-8
+
+def ruido(imagen_binaria, nombre_archivo, carpeta_salida, cantidad_ruido=1024):
+    """Guarda el string base64 en un archivo binario con ruido."""
+    os.makedirs(carpeta_salida, exist_ok=True)  # Crea la carpeta si no existe
+    ruta_archivo = os.path.join(carpeta_salida, nombre_archivo + ".null")  # Añade la extensión ".imgbin"
+    
+    ruido = bytes(random.randint(0, 255) for _ in range(cantidad_ruido))
+    datos_con_ruido = ruido + imagen_binaria + ruido
+    
+    with open(ruta_archivo, "wb") as archivo_binario:
+        archivo_binario.write(datos_con_ruido)
+
+
+
+def procesar_imagenes(carpeta_imagenes, carpeta_salida):
+    """Procesa todas las imágenes de una carpeta."""
+    for nombre_archivo in os.listdir(carpeta_imagenes):
+        if nombre_archivo.endswith((".jpg", ".jpeg", ".png")):
+            ruta_imagen = os.path.join(carpeta_imagenes, nombre_archivo)
+            imagen_binaria = imagen_a_base64(ruta_imagen)
+            ruido(imagen_binaria, nombre_archivo.split(".")[0]  , carpeta_salida)
+
+
+
+if __name__ == "__main__":
+    carpeta_imagenes = "../data/img"  # Nombre de la carpeta con tus imágenes
+    carpeta_base64 = "../mierda/ruidoImg"  # Nombre de la carpeta para los archivos base64
+    # Convertir las imágenes a base64 y guardar en carpetas separadas
+    procesar_imagenes(carpeta_imagenes, carpeta_base64)
---
+++ src/eliminar_ruido_Img.py
@@ -0,0 +1,32 @@
+import base64
+import os
+import random
+from PIL import Image
+
+def base64(ruta_archivo_binario, nombre_archivo, carpeta_salida, cantidad_ruido=1024):
+    """Convierte un string base64 a una imagen y elimina el ruido."""
+    os.makedirs(carpeta_salida, exist_ok=True)
+    ruta_archivo = os.path.join(carpeta_salida, nombre_archivo + ".jpeg")  # Agrega extensión
+    with open(ruta_archivo_binario, "rb") as archivo_binario:
+        datos_con_ruido = archivo_binario.read()
+        
+        # Elimina el ruido
+        imagen_binaria = datos_con_ruido[cantidad_ruido:-cantidad_ruido]
+        
+        with open(ruta_archivo, "wb") as imagen_archivo:
+            imagen_archivo.write(imagen_binaria)
+
+def recuperar_imagenes(carpeta_base64, carpeta_salida):
+    """Recupera imágenes desde los archivos base64."""
+    for nombre_archivo in os.listdir(carpeta_base64):
+        # Elimina la extensión de la imagen original
+        nombre_archivo_sin_ext = os.path.splitext(nombre_archivo)[0]
+        ruta_archivo = os.path.join(carpeta_base64, nombre_archivo)
+        base64(ruta_archivo, nombre_archivo_sin_ext, carpeta_salida, cantidad_ruido=1024)
+
+if __name__ == "__main__":
+    carpeta_base64 = "../mierda/ruidoImg"  # Nombre de la carpeta para los archivos base64
+    carpeta_recuperadas = "../mierda/eliminarRuidoImg"   # Nombre de la carpeta para las imágenes recuperadas
+
+    # Recuperar las imágenes desde los archivos base64
+    recuperar_imagenes(carpeta_base64, carpeta_recuperadas)
