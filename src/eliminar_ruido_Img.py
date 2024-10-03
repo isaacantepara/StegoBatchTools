3198666ba47a81fcfff936b015859afc3473b315
Isaac Antepara <69361351+isaacanteparac@users.noreply.github.com>
2024-10-03 17:59:36 -0500

    recuperar imagen original

--- src/eliminar_ruido_Img.py
+++ src/eliminar_ruido_Img.py
@@ -3,7 +3,7 @@ import os
 import random
 from PIL import Image
 
-def base64(ruta_archivo_binario, nombre_archivo, carpeta_salida, cantidad_ruido=1024):
+def imagen_(ruta_archivo_binario, nombre_archivo, carpeta_salida, cantidad_ruido=1024):
     """Convierte un string base64 a una imagen y elimina el ruido."""
     os.makedirs(carpeta_salida, exist_ok=True)
     ruta_archivo = os.path.join(carpeta_salida, nombre_archivo + ".jpeg")  # Agrega extensión
@@ -16,17 +16,17 @@ def base64(ruta_archivo_binario, nombre_archivo, carpeta_salida, cantidad_ruido=
         with open(ruta_archivo, "wb") as imagen_archivo:
             imagen_archivo.write(imagen_binaria)
 
-def recuperar_imagenes(carpeta_base64, carpeta_salida):
+def recuperacion(carpeta_base64, carpeta_salida):
     """Recupera imágenes desde los archivos base64."""
     for nombre_archivo in os.listdir(carpeta_base64):
         # Elimina la extensión de la imagen original
         nombre_archivo_sin_ext = os.path.splitext(nombre_archivo)[0]
         ruta_archivo = os.path.join(carpeta_base64, nombre_archivo)
-        base64(ruta_archivo, nombre_archivo_sin_ext, carpeta_salida, cantidad_ruido=1024)
+        imagen_(ruta_archivo, nombre_archivo_sin_ext, carpeta_salida, cantidad_ruido=1024)
 
 if __name__ == "__main__":
     carpeta_base64 = "../mierda/ruidoImg"  # Nombre de la carpeta para los archivos base64
     carpeta_recuperadas = "../mierda/eliminarRuidoImg"   # Nombre de la carpeta para las imágenes recuperadas
 
     # Recuperar las imágenes desde los archivos base64
-    recuperar_imagenes(carpeta_base64, carpeta_recuperadas)
+    recuperacion(carpeta_base64, carpeta_recuperadas)
